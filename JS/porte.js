0d15a2e84ac9b9531d66204d3210171dcab196ca
romaink30 <kmieckowiakr@gmail.com>
2024-09-19 12:28:06 +0200

    animation de la porte

--- JS/porte.js
+++ JS/porte.js
@@ -8,12 +8,18 @@ document.getElementById('unlockBtn').addEventListener('click', function() {
     const code = document.getElementById('code').value;
     const message = document.getElementById('message');
     
-
+    // Le mot de passe est "train"
     if (code === "train") {
         message.style.color = 'green';
         message.textContent = 'Bravo, la porte est déverrouillée !';
         document.querySelector('.door').classList.add('open');  // Ajouter la classe 'open'
         document.getElementById('codeEntry').style.display = 'none';
+
+        // Redirection vers une autre page après une petite attente pour que l'animation soit visible
+        setTimeout(function() {
+            window.location.replace('/html/quizz2.html');  // Remplacer par le chemin correct
+        }, 1000);  // Attendre 1 seconde avant la redirection
+        
     } else {
         message.style.color = 'red';
         message.textContent = 'Code incorrect. Essayez encore.';
